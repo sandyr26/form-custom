9457893ac221a909fe6074dd2e449dd7059f3c29
Sandy <sandy.ramiah@prodigious.mu>
2022-07-11 12:30:33 +0400

    added development stage

---
+++ src/components/atoms/RowHeaderMain.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+export default function RowHeaderMain() {
+  return (
+    <div className='sticky top-0 flex px-8 py-8 mb-8 bg-white border-b-2 drop-shadow-xl'>
+        <div className='w-[100%] font-light'>
+            <div>
+                <div className='text-xl font-normal pb-2'>Solar Photovoltaic System Value Chain: 1 - Development Stage</div>
+                <div>
+                    This stage is also known as the Feasibility study stage and it includes acquiring land, permitting, financing, initial feasibility studies, and all activities completed prior to the project’s detailed design or procurement.
+                </div> font-extralight
+                <div className='flex gap-4 pt-8'>
+                    <div className='flex flex-col items-center border-2 border-blue-500 w-1/6 h-24'>
+                        <div className=' w-full h-8 text-black flex justify-center items-center font-semibold'>1</div>
+                        <div className='h-18 pt-2 text-black text-center font-semibold'>Development</div>
+                    </div>
+                    <div className='flex flex-col items-center bg-blue-500 border-2 border-blue-500 w-1/6 h-24'>
+                        <div className='bg-blue-500 w-full h-8 text-white flex justify-center items-center font-bold'>2</div>
+                        <div className='h-18 pt-2 text-white text-center font-extralight'>Design</div>
+                    </div>
+                    <div className='flex flex-col items-center bg-blue-500 border-2 border-blue-500 w-1/6 h-24'>
+                        <div className='bg-blue-500 w-full h-8 text-white flex justify-center items-center font-bold'>3</div>
+                        <div className='h-18 pt-2 text-white text-center font-extralight'>Manufacturing/ Import</div>
+                    </div>
+                    <div className='flex flex-col items-center bg-blue-500 border-2 border-blue-500 w-1/6 h-24'>
+                        <div className='bg-blue-500 w-full h-8 text-white flex justify-center items-center font-bold'>4</div>
+                        <div className='h-18 pt-2 text-white text-center font-extralight'>Installation/ Commissioning</div>
+                    </div>
+                    <div className='flex flex-col items-center bg-blue-500 border-2 border-blue-500 w-1/6 h-24'>
+                        <div className='bg-blue-500 w-full h-8 text-white flex justify-center items-center font-bold'>5</div>
+                        <div className='h-18 pt-2 text-white text-center font-extralight'>Operation & Maintenance</div>
+                    </div>
+                    <div className='flex flex-col items-center bg-blue-500 border-2 border-blue-500 w-1/6 h-24'>
+                        <div className='bg-blue-500 w-full h-8 text-white flex justify-center items-center font-bold'>6</div>
+                        <div className='h-18 pt-2 text-white text-center'>Decomissioning</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
--- src/components/molecules/Table.jsx
+++ src/components/molecules/Table.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Row from '../atoms/RowData'
 import RowHeader from '../atoms/RowHeader'
+import RowHeaderMain from '../atoms/RowHeaderMain'
 
 export default function Table(props) {
 
   return (
     <div className='flex flex-col w-[56rem] my-8 '>
+        <RowHeaderMain />
         <RowHeader/>
         <Row  handleChangeProbability={props.handleChangeProbability} handleChangeImpact={props.handleChangeImpact} handleChangeSeverity={props.handleChangeSeverity}/>
     </div>
